d5f71fc444b05b50173b773816a94918bc5ffc36
ChainArts <maxroll54@gmail.com>
2025-02-05 21:49:13 +0100

    refactor db process to terminate properly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,27 @@
 
 use std::env;
 use std::os::windows::process::CommandExt;
-use std::process::{Command, Stdio};
+use std::process::{Command,Child, Stdio};
 use std::thread;
 use std::time::Duration;
+use std::sync::LazyLock;
 
-use surrealdb::engine::any;
-use surrealdb::engine::remote::ws::Ws;
+use surrealdb::engine::remote::ws::{Client,Ws, Wss};
 use surrealdb::opt::auth::Root;
 use surrealdb::Surreal;
 use dotenv::dotenv;
 use winapi::um::winbase::CREATE_NO_WINDOW;
 
-#[tokio::main]
-async fn main() -> surrealdb::Result<()> {
-    dotenv().ok();
+static LOCAL_DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
+static EXTERNAL_DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
 
-    println!("Starting local SurrealDB server");
-    thread::spawn(|| {
-        let mut _child = Command::new("surreal")
+struct DatabaseProcess {
+    child: Child,
+}
+
+impl DatabaseProcess {
+    fn new() -> Self {
+        let child = Command::new("surreal")
             .args(&[
                 "start",
                 "--user", &env::var("LOCAL_USERNAME").expect("LOCAL_USERNAME not set"),
@@ -30,26 +33,41 @@ async fn main() -> surrealdb::Result<()> {
             ])
             .stdout(Stdio::null()) // Redirect stdout to null
             .stderr(Stdio::null()) // Redirect stderr to null
-            .creation_flags(CREATE_NO_WINDOW)
+            .creation_flags(CREATE_NO_WINDOW) // Prevent shell window from opening
             .spawn()
             .expect("Failed to start SurrealDB local server");
 
-         let status = _child.wait().expect("Failed to wait on SurrealDB process");
-            println!("SurrealDB process exited with status: {:?}", status);
-    });
+        DatabaseProcess { child }
+    }
+}
+
+impl Drop for DatabaseProcess {
+    fn drop(&mut self) {
+        self.child.kill().expect("Failed to kill SurrealDB process");
+        println!("SurrealDB process terminated");
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    dotenv().ok();
+
+    println!("Starting local SurrealDB server");
+    let _db_process = DatabaseProcess::new();
 
     // Allow some time for the local SurrealDB process to start up.
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_millis(250));
     println!("Local SurrealDB server started");
 
 
     // Connect to the server
     let local_username = env::var("LOCAL_USERNAME").expect("LOCAL_USERNAME not set");
     let local_password = env::var("LOCAL_PASSWORD").expect("LOCAL_PASSWORD not set");
-    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    LOCAL_DB.connect::<Ws>("127.0.0.1:8000").await?;
 
     // Signin as a namespace, database, or root user
-    db.signin(Root {
+    LOCAL_DB.signin(Root {
         username: &local_username,
         password: &local_password,
     })
@@ -60,14 +78,23 @@ async fn main() -> surrealdb::Result<()> {
     let external_username = env::var("EXTERNAL_USERNAME").expect("EXTERNAL_USERNAME not set");
     let external_password = env::var("EXTERNAL_PASSWORD").expect("EXTERNAL_PASSWORD not set");
 
-    let external_db = any::connect("wss://lifequest-db-06acp6cd6du4v8mbo26bhlv1fo.aws-euw1.surreal.cloud").await?;
+    EXTERNAL_DB.connect::<Wss>("lifequest-db-06acp6cd6du4v8mbo26bhlv1fo.aws-euw1.surreal.cloud").await?;
     println!("Connected to external db");
-    external_db.signin(Root {
+    EXTERNAL_DB.signin(Root {
         username: &external_username,
         password: &external_password,
     }).await?;
 
-    external_db.use_ns("dev").use_db("lifequest").await?;
+    EXTERNAL_DB.use_ns("dev").use_db("lifequest").await?;
+
+    // Test Local DB connection using get to status
+    let status = LOCAL_DB.version().await?;
+    println!("Local DB status: {:?}", status);
+
+    // Test External DB connection using get to status
+    let status = EXTERNAL_DB.version().await?;
+    println!("External DB status: {:?}", status);
+
     
     lifequest_lib::run();
     Ok(())
