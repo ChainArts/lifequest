21afffc8ceeb2ec01f7aa7bb72e5da0d02bba4c9
ChainArts <maxroll54@gmail.com>
2025-02-11 18:03:02 +0100

    add font for export

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/Typo.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "./lib/ThemeContext";
 
