7a30afbee0a06f81b30c0f339f2ca8ad2a1ecc02
Sandra Scheipl <sandra.scheipl@gmail.com>
2025-02-13 00:59:06 +0100

    add animation to circular progress

--- src/components/atoms/CircularProgress/CircularProgress.tsx
+++ src/components/atoms/CircularProgress/CircularProgress.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./CircularProgress.scss";
 
 type CircularProgressProps = {
@@ -5,25 +6,54 @@ type CircularProgressProps = {
 };
 
 const CircularProgress = ({ progress }: CircularProgressProps) => {
+    const [animatedNumber, setAnimatedNumber] = useState(0);
+    const [previousProgress, setPreviousProgress] = useState(0);
+
+    if (progress > 100) {
+        progress = 100;
+    }
+
+    if (progress < 0) {
+        progress = 0;
+    }
+
     const size = 80;
     const radius = size - 10;
 
     const startAngle = -200;
     const maxRadius = 60;
 
-    const calculatedProgress = () => {
-        if (progress > 100) {
-            progress = 100;
-        }
-
-        if (progress < 0) {
-            return 0;
-        }
-        return maxRadius * (progress / 100);
-    };
-
     const dashArray = 2 * Math.PI * radius;
-    const dashOffset = dashArray - (dashArray * calculatedProgress()) / 100;
+    const dashOffset =
+        dashArray - (dashArray * (maxRadius * (progress / 100))) / 100;
+
+    useEffect(() => {
+        let start = previousProgress;
+        let end = progress;
+        const duration = 1000; // Duration of the animation in ms
+        const startTimestamp = performance.now();
+
+        const animate = (timestamp: number) => {
+            const progressTime = Math.min(
+                (timestamp - startTimestamp) / duration,
+                1
+            );
+            const easedProgress = easeOutCubic(progressTime);
+            const currentNumber = Math.round(
+                start + (end - start) * easedProgress
+            );
+            setAnimatedNumber(currentNumber);
+
+            if (progressTime < 1) {
+                requestAnimationFrame(animate);
+            }
+        };
+
+        requestAnimationFrame(animate);
+        setPreviousProgress(progress); // Remember the current progress as the last state
+    }, [progress]);
+
+    const easeOutCubic = (t: number) => 1 - Math.pow(1 - t, 3);
 
     return (
         <svg width={size * 2} height={size * 1.5}>
@@ -68,14 +98,14 @@ const CircularProgress = ({ progress }: CircularProgressProps) => {
                 y={size}
                 textAnchor="middle"
                 className="circle-progress__number"
-            >{`${progress}%`}</text>
+            >{`${animatedNumber}%`}</text>
             <text
                 x={size}
                 y={size + 20}
                 textAnchor="middle"
                 className="fst--upper-heading purple"
             >
-                finshed
+                finished
             </text>
         </svg>
     );
