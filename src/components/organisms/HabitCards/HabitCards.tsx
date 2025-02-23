7e076fc04ee3dba74b66abb6c46634dce7800db0
Sandra Scheipl <sandra.scheipl@gmail.com>
2025-02-23 15:31:44 +0100

    clean-up

--- src/components/organisms/HabitCards/HabitCards.tsx
+++ src/components/organisms/HabitCards/HabitCards.tsx
@@ -2,23 +2,39 @@ import { IconContext } from "react-icons";
 import Headline from "../../atoms/Headline/Headline";
 import HabitCard from "../../molecules/HabitCard/HabitCard";
 import "./HabitCards.scss";
+import { Sheet } from "react-modal-sheet";
+import { useState } from "react";
 
 const HabitCards = () => {
+    const [isOpen, setOpen] = useState(false);
     return (
-        <section className="container">
-            <IconContext.Provider value={{ className: "habit-cards__icons" }}>
-                <div className="habit-cards__icons">
-                    <Headline level={1} style="section">
-                        Habits
-                    </Headline>
-                    <button className="fst_headline gray">create</button>
-                </div>
-                <div className="habit-cards__list">
-                    <HabitCard />
-                    <HabitCard />
-                </div>
-            </IconContext.Provider>
-        </section>
+        <>
+            <section className="container">
+                <IconContext.Provider value={{ className: "habit-cards__icons" }}>
+                    <div className="title-and-button">
+                        <Headline level={1} style="section">
+                            Habits
+                        </Headline>
+                        <button onClick={() => setOpen(true)}>create</button>
+                    </div>
+                    <div className="habit-cards__list">
+                        <HabitCard />
+                        <HabitCard />
+                    </div>
+                </IconContext.Provider>
+            </section>
+            <section className="container">
+                <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+                    <Sheet.Container>
+                        <Sheet.Header />
+                        <Sheet.Content>
+                            <Sheet.Scroller>hi</Sheet.Scroller>
+                        </Sheet.Content>
+                    </Sheet.Container>
+                    <Sheet.Backdrop />
+                </Sheet>
+            </section>
+        </>
     );
 };
 
--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -1,26 +1,11 @@
 import pageVariants from "../components/atoms/PageTransition/PageTransition";
 import { motion } from "motion/react";
-import { useState } from "react";
-import { Sheet } from "react-modal-sheet";
 import HabitCards from "../components/organisms/HabitCards/HabitCards";
 
 const Habits = () => {
-    const [isOpen, setOpen] = useState(false);
     return (
         <motion.main initial="initial" animate="in" exit="out" variants={pageVariants}>
             <HabitCards />
-
-            <button onClick={() => setOpen(true)}>Open sheet</button>
-
-            <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
-                <Sheet.Container>
-                    <Sheet.Header />
-                    <Sheet.Content>
-                        <Sheet.Scroller>hi</Sheet.Scroller>
-                    </Sheet.Content>
-                </Sheet.Container>
-                <Sheet.Backdrop />
-            </Sheet>
         </motion.main>
     );
 };
