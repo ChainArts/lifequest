fc79ab924c8bcc6bad0c48f853c9cc7962f0975e
ChainArts <maxroll54@gmail.com>
2025-02-28 01:02:14 +0100

    update island

--- src/components/organisms/IslandHeader/IslandHeader.tsx
+++ src/components/organisms/IslandHeader/IslandHeader.tsx
@@ -2,22 +2,40 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Stats } from "@react-three/drei";
 import "./IslandHeader.scss";
 import * as THREE from "three";
+import { useMemo } from "react";
 
-const Model = (props: any) => {
-    const { scene } = useGLTF("/src/assets/models/island.glb"); // update the path to your GLB file
+const Island = (props: any) => {
+    const scene = useMemo(() => {
+        const { scene } = useGLTF("/src/assets/models/island.glb");
+        // scale up the island by 10
+        scene.scale.set(10, 10, 10);
+        return scene;
+    }, []);
+
     return <primitive object={scene} {...props} />;
 };
 
 const IslandHeader = () => {
+    const skybox = useMemo(() => {
+        const loader = new THREE.TextureLoader();
+        return loader.load("/src/assets/models/skybox.png");
+    }, []);
     return (
         <header className="island-header">
-            <Canvas className="island-header__canvas" onCreated={({ scene }) => (scene.background = new THREE.Color("#9da8e6"))}>
+            <Canvas
+                className="island-header__canvas"
+                onCreated={({ gl, scene }) => {
+                    const rt = new THREE.WebGLCubeRenderTarget(skybox.image.height);
+                    rt.fromEquirectangularTexture(gl, skybox);
+                    scene.background = rt.texture;
+                }}
+            >
                 <Stats />
                 <OrbitControls />
                 <ambientLight intensity={Math.PI / 2} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                 <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                <Model position={[0, 0, 0]} />
+                <Island position={[0, 0, 0]} />
             </Canvas>
         </header>
     );
