9dfce5ae9e331bd87088741ce1a8a3417335877f
Sandra Scheipl <sandra.scheipl@gmail.com>
2025-05-14 01:22:58 +0200

    add recharts

--- src/components/organisms/HabitGraph/HabitGraph.tsx
+++ src/components/organisms/HabitGraph/HabitGraph.tsx
@@ -1,8 +1,28 @@
+import { Line, Tooltip, XAxis, YAxis } from "recharts";
 import Headline from "../../atoms/Headline/Headline";
 import Card from "../../molecules/Card/Card";
 import "./HabitGraph.scss";
+import { CartesianGrid, LineChart, ResponsiveContainer } from "recharts";
 
 const HabitGraph = () => {
+    const data = [
+        { date: "2025-01-01", data: 12 },
+        { date: "2025-01-02", data: 10 },
+        { date: "2025-01-03", data: 8 },
+        { date: "2025-01-04", data: 15 },
+        { date: "2025-01-05", data: 20 },
+        { date: "2025-01-06", data: 25 },
+        { date: "2025-01-07", data: 30 },
+        { date: "2025-01-08", data: 35 },
+        { date: "2025-01-09", data: 40 },
+        { date: "2025-01-10", data: 45 },
+        { date: "2025-01-11", data: 50 },
+        { date: "2025-01-12", data: 55 },
+        { date: "2025-01-13", data: 60 },
+        { date: "2025-01-14", data: 65 },
+        { date: "2025-01-15", data: 70 },
+    ];
+
     return (
         <>
             <div className="title-and-button">
@@ -10,9 +30,16 @@ const HabitGraph = () => {
                     Tracking Graph
                 </Headline>
             </div>
-            <Card className="habit-graph__card">
-                Soon TM
-                <div className="habit-graph__card__content">{/* Graph will be implemented here */}</div>
+            <Card className="habit-graph__card" style={{ height: 320 }}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianGrid opacity={0.5} vertical={false} />
+                        <XAxis dataKey="date" scale="point" tickLine={false} axisLine={false} />
+                        <YAxis />
+                        <Tooltip />
+                        <Line type="linear" dataKey="data" stroke="#8884d8" />
+                    </LineChart>
+                </ResponsiveContainer>
             </Card>
         </>
     );
