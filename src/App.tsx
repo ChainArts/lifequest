41723bb75f7b6b906931339ae3ef91c30fd55a8b
ChainArts <maxroll54@gmail.com>
2025-05-14 15:40:45 +0200

    add toastify

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,33 @@ import { HabitsProvider } from "./lib/HabitsContext";
 import { UserProvider } from "./lib/UserContext";
 import UserBar from "./components/organisms/UserBar/UserBar";
 import { PopOverProvider } from "./lib/PopOverContext";
+import { ToastContainer, cssTransition } from "react-toastify";
+
+const ToastTransition = cssTransition({
+    
+    enter: "Toastify_fadeIn",
+    exit: "Toastify_fadeOut",
+    
+});
 
 function App() {
     const location = useLocation();
     return (
         <UserProvider>
             <HabitsProvider>
+                <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={ToastTransition}
+            />
                 <UserBar />
                 <PopOverProvider>
                     <IslandContainer location={location.pathname} />
