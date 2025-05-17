e194a99317bb363dd588f513b1808a949913b9a0
Sandra Scheipl <sandra.scheipl@gmail.com>
2025-05-17 18:26:28 +0200

    fix habits calendar

--- src/components/organisms/StreakCalender/StreakCalender.tsx
+++ src/components/organisms/StreakCalender/StreakCalender.tsx
@@ -1,13 +1,19 @@
+import { useHabits } from "../../../lib/HabitsContext";
+import { useUser } from "../../../lib/UserContext";
 import StreakDay from "../../atoms/StreakDay/StreakDay";
 import "./StreakCalender.scss";
 
 type StreakCalenderProps = {
     streak: number;
+    todayCompleted: boolean;
 };
 
-const StreakCalender = ({ streak }: StreakCalenderProps) => {
+const StreakCalender = ({ streak, todayCompleted }: StreakCalenderProps) => {
     const today = new Date().getDay(); // 0 = Sunday, …, 6 = Saturday
     const days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+
+    console.log("todayCompleted", todayCompleted);
+    console.log("streak", streak);
 
     // convert JS Sunday=0 → index 6, Monday=1 → 0, … Saturday=6 → 5
     const dayIndex = (today + 6) % 7;
@@ -21,9 +27,9 @@ const StreakCalender = ({ streak }: StreakCalenderProps) => {
         <div className="container">
             <div className="streak-progress__container">
                 {orderedDays.map((day, idx) => {
-                    const isTodayCell = idx === offset; // today at position 0 when streak ≤1
-                    const completed = idx < streak; // mark all < streak as done
-                    return <StreakDay key={idx} day={day} isToday={isTodayCell} isCompleted={completed} />;
+                    const isTodayCell = idx === offset;
+                    const completed = idx < streak - 1;
+                    return <StreakDay key={idx} day={day} isToday={isTodayCell} isCompleted={(isTodayCell && todayCompleted) || completed} />;
                 })}
             </div>
         </div>
